1a7d580685720a5601726a364bc822bb1804b2a4
Deepashivadas <deepashivadas1992@gmail.com>
2022-05-24 21:06:23 +0530

    Demo code

---
+++ src/org/cts/com/Employee.java
@@ -0,0 +1,19 @@
+package org.cts.com;
+
+public class Employee {
+	
+	public void empId() {
+System.out.println( "employee id is 673");
+	}
+	
+	
+	public static void main(String[] args) {
+		Employee a= new Employee();
+		a.empId();
+		
+	}
+
+	}
+
+
+
